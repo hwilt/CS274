fc1c27b017b43c6d0669126b95a7e40a9c0471c6
hwilt <henrywwilt@gmail.com>
2021-02-01 20:41:30 -0500

    Write and Read File is Working
    also checking if the input is proper or not

--- HW/src/HW0.java
+++ HW/src/HW0.java
@@ -8,10 +8,17 @@ import java.util.*;
 
 public class HW0 {
     //This will hold the data of the file, line by line
-    public static ArrayList<String> FILE_DATA = new ArrayList<>(); 
+    public static ArrayList<String> FILE_DATA = new ArrayList<>();
+    //This will hold the converted data
+    public static ArrayList<String> CONVERTED_DATA = new ArrayList<>();
+    //This is the index of the second comma in the line data
+    public static final int SECOND_COMMA = 3; 
+    //This is the index of the first comma in the line data
+    public static final int FIRST_COMA = 1; 
+    
     
     /**
-     * This method will allow us to read the file "conversion_in.txt"
+     * This method will allow us to read from file "conversion_in.txt"
      * then put the data in the file line by line in an arrayList to
      * use more conveniently.
      */
@@ -21,7 +28,14 @@ public class HW0 {
             Scanner reader = new Scanner(file);
             while(reader.hasNextLine()){
                 String data = reader.nextLine();
-                FILE_DATA.add(data);
+                if(isValidBase(data)){
+                    FILE_DATA.add(data);
+                }
+                else{
+                    String[] splits = data.split(",");
+                    data = splits[0] + "," + splits[1] + ",improper input";
+                    FILE_DATA.add(data);
+                }
             }
             reader.close();
         }catch (FileNotFoundException e) {
@@ -30,10 +44,18 @@ public class HW0 {
         }
     }
     
+    /**
+     * This method will allow us to write into file "conversion_out.txt"
+     * 
+     * TODO: PRINT OUT THE VALUES CONVERTED INTO A TABLE
+     */
     public static void writeFile(){
         try {
             FileWriter writer = new FileWriter("conversion_out.txt");
-            writer.write("Files in Java might be tricky, but it is fun enough!");
+            writer.write("Converter Results\n");
+            for(String i : FILE_DATA){
+                writer.write(i + "\n");
+            }
             writer.close();
         } catch (IOException e) {
             System.out.println("An error occurred.");
@@ -41,17 +63,64 @@ public class HW0 {
         } 
     }
     
+    /**
+     * This method checks if the line data has an improper input like for
+     * b, h, "298" B = binary which in the input "298" would not be proper
+     * so return false.
+     * 
+     * @param data the line data to check if the input is improper
+     * @return boolean isValid this will be true if the input is proper 
+     */
+    public static boolean isValidBase(String data){
+        boolean isValid = true;
+        String base = data.substring(0, FIRST_COMA);
+        String value = data.substring(SECOND_COMMA + 1);
+        switch(base){ //checking each case Binary, Hexadecimal, and Decimal
+            case "B": //Binary
+                for(int i = 0; i < value.length(); i++){
+                    int temp = value.charAt(i);
+                    if(temp != '0' && temp != '1'){
+                        return false;
+                    }
+                }
+                break;
+            case "H": //Hexadecimal
+                for(int i = 0; i < value.length(); i++){
+                    int temp = value.charAt(i);
+                    boolean isTrue = temp != '0' && temp != '1' && temp != '2' && 
+                            temp != '3' && temp != '4' && temp != '5' &&
+                            temp != '6' && temp != '7' && temp != '8' && temp != '9' 
+                            && temp != 'A' && temp != 'B' && temp != 'C' && temp != 'D'
+                            && temp != 'E' && temp != 'F';
+                    if(isTrue){ //if any of those numbers are found then it will be an improper input
+                        return false;
+                    }
+                }
+                break;
+            case "D": //Decimal
+                for(int i = 0; i < value.length(); i++){
+                    int temp = value.charAt(i);
+                    boolean isTrue = temp != '0' && temp != '1' && temp != '2' && 
+                            temp != '3' && temp != '4' && temp != '5' &&
+                            temp != '6' && temp != '7' && temp != '8' && temp != '9';
+                    if(isTrue){ //if any of those numbers are found then it will be an improper input
+                        return false;
+                    }
+                }
+                break;
+        }
+        return isValid;
+    }
     
+    public static void convertValue(String data){
+        
+    }
     
     public static void main(String[] args){
-        try {
-            FileWriter writer = new FileWriter("conversion_out.txt");
-            writer.write("Files in Java might be tricky, but it is fun enough!");
-            writer.close();
-            System.out.println("Successfully wrote to the file.");
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        } 
+        readFile();
+        for(String i: FILE_DATA){
+            System.out.println(i);
+        }
+        writeFile();
     }
 }
