b144c6dc7832c9e2e1d5d56cf83ea25d2af1266f
hwilt <henrywwilt@gmail.com>
2021-02-21 13:17:09 -0500

    Hollow + Filled done

--- HW2/example.cpp
+++ HW2/example.cpp
@@ -2,30 +2,67 @@
 #include <iostream>
 using namespace std;
 
-void printTriangle(int rows){
-    int space;
-
-    for(int i = 1, k = 0; i <= rows; ++i, k = 0){
-        for(space = 1; space <= rows - i; ++space){
-            cout << " ";
+void printTriangle(int rows, char ch){
+    int i, j, k = 0;
+    bool isLast = false; 
+    for (i = 1; i <= rows; i++)
+    { 
+        if(i == rows){
+            isLast = true;
         }
-        while(k != 2*i-1){
-            cout << "*";
-            ++k;
+        if(isLast){
+            for (i = 0; i < 2 * rows - 1; i++) { 
+                cout << ch; 
+            }
+            for(int spaces = 0; spaces <= rows; spaces++){
+                cout << " ";
+            }
+            for (i = 0; i < 2 * rows - 1; i++) { 
+                cout << ch; 
+            }
         }
-        cout << endl;
-    }
+        else{
+            for (j = i; j < rows; j++) { 
+                cout << " "; 
+            }
+            while (k != (2 * i - 1)) { 
+                if (k == 0 || k == 2 * i - 2) 
+                    cout << ch; 
+                else
+                    cout << " "; 
+                k++; 
+                ; 
+            } 
+            k = 0;
+            //isHollow = false; 
+            for (j = i; j < rows * 2; j++) { 
+                cout << " "; 
+            }
+            for(int spaces = i; spaces <= rows; spaces++){
+                cout << " ";
+            }
+            while(k != (2 * i - 1)){
+                cout << ch;
+                k++;
+            }
+            k = 0;
+            cout << endl;
+        }
+    } 
 }
 
 int main(){
     int n = 0;
+    char character = '*';
     cout << "Enter number of rows: ";
     cin >> n;
+    cout << "Enter a character for the pyramid: ";
+    cin >> character;
     if(n <= 0){
         cout << "Choose a bigger number!";
     }
     else{
-        printTriangle(n);
+        printTriangle(n, character);
     }
     return 0;
 }
